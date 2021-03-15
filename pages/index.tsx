a5034a13911cd7e476a1d256dd5dec3c0ec4ec67
MoisesSantana <moisaant@gmail.com>
2021-03-15 12:30:36 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hara's Burger</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        oi
+      </main>
+
+      <footer>
+        xau
+      </footer>
+    </div>
+  )
+}
