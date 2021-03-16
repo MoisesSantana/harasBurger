14e9a2c1761f56dfc5982f7fe8701bff11494767
MoisesSantana <moisaant@gmail.com>
2021-03-16 00:58:20 -0300

    aplicação configurada

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Hara&apos;s Burger</title>
+    </Head>
+          
+  </div>
+);
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #832B00;
+    color: #FAFFEB;
+    font-family: 'Poppins', sans-serif;
+  }
+`;
