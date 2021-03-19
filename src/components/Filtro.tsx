68e9eaf92bd88df0e35f8ebda370b79e6d41592b
MoisesSantana <moisaant@gmail.com>
2021-03-19 00:12:25 -0300

    importação do Provider no _app

--- src/components/Filtro.tsx
+++ src/components/Filtro.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { filtros } from '../data/listaDeAlimentos';
 import FiltroContainer from '../styles/components/FiltroComponent';
 
-const Filtro: React.FC = () => (
-  <FiltroContainer
-    name="filtra-categoria"
-    id="filtra-categoria"
-  >
-    {
-      filtros.map(({ nome }) => (
-        <option
-          value={nome}
-          key={nome}
-        >
-          {nome}
-        </option>
-      ))
-    }
-  </FiltroContainer>
-);
+const Filtro: React.FC = () => {
+  const [filtroAtual, setFiltroAtual] = useState('Todos');
+
+  return (
+    <FiltroContainer
+      name="filtra-categoria"
+      id="filtra-categoria"
+    >
+      {
+        filtros.map(({ nome }) => (
+          <option
+            value={nome}
+            key={nome}
+          >
+            {nome}
+          </option>
+        ))
+      }
+    </FiltroContainer>
+  );
+}
+
 
 export default Filtro;
