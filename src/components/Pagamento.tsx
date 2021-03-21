6a9079b372aca970fc68abab796278f245fcfc11
MoisesSantana <moisaant@gmail.com>
2021-03-21 02:46:08 -0300

    componente Pagamento estilização básica

--- src/components/Pagamento.tsx
+++ src/components/Pagamento.tsx
@@ -1,38 +1,50 @@
 import React from 'react';
+import PagamentoContainer from '../styles/components/PagamentoComponent';
 
-const Pagamento: React.FC = () => (
-  <div>
-    <div>
-      <input
-        type="radio"
-        name="formaDePagamento"
-        id="dinheiro"
-      />
-      <label htmlFor="dinheiro">
-        Dinheiro
+const Pagamento: React.FC = () => {
+  const troco = true;
+  return (
+    <PagamentoContainer>
+      <div>
+
+        <input
+          type="radio"
+          name="formaDePagamento"
+          id="dinheiro"
+        />
+        <label htmlFor="dinheiro">
+          Dinheiro
+        </label>
+        <input
+          type="radio"
+          name="formaDePagamento"
+          id="debito"
+        />
+        <label htmlFor="debito">
+          Débito
+        </label>
+        <input
+          type="radio"
+          name="formaDePagamento"
+          id="credito"
+        />
+        <label htmlFor="credito">
+          Crédito
+        </label>
+      </div>
+      <label
+        className="troco"
+        htmlFor="troco"
+      >
+        Vai precisar de troco? Para quanto?
+        <input
+          type="text"
+          name="troco"
+          id="troco"
+        />
       </label>
-    </div>
-    <div>
-      <input
-        type="radio"
-        name="formaDePagamento"
-        id="debito"
-      />
-      <label htmlFor="debito">
-        Débito
-      </label>
-    </div>
-    <div>
-      <input
-        type="radio"
-        name="formaDePagamento"
-        id="credito"
-      />
-      <label htmlFor="credito">
-        Crédito
-      </label>
-    </div>
-  </div>
-);
+    </PagamentoContainer>
+  );
+}
 
 export default Pagamento;
